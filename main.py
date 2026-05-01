b28118fdfdb0dfe626f031a4454766dc81dff15c
Shubham Raj <shubmrj@gmail.com>
2026-05-01 15:23:41 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "simple_rnn.keras")
+
+model = load_model(model_path)
+
+st.title("IMDB Sentiment Analysis")
+
+text = st.text_area("Enter review")
+
+if st.button("Predict"):
+    st.write("Result here")
